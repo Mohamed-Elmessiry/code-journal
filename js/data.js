d6d50d6843b61e2b06271cb9391a253ce3e7e715
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-03-30 17:36:04 -0400

    modified css

--- js/data.js
+++ js/data.js
@@ -6,3 +6,16 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousEntriesJson = localStorage.getItem('javascript-local-storage');
+if (previousEntriesJson !== null) {
+  data = JSON.parse(previousEntriesJson);
+}
+
+window.addEventListener('beforeunload', stringnifyNstore);
+
+function stringnifyNstore(event) {
+  var dataJson = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJson);
+
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 /* global data */
 /* exported data */
+var photo = document.querySelector('.image');
+var pUrl = document.querySelector('.photo-url');
+pUrl.addEventListener('input', function (event) {
+  photo.src = event.target.value;
+});
+
+var formSelector = document.querySelector('.main-form');
+formSelector.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var values = {
+    title: formSelector.elements.title.value,
+    url: formSelector.elements.url.value,
+    textbox: formSelector.elements.notes.value,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.push(values);
+  photo.src = './images/placeholder-image-square.jpg';
+  formSelector.reset();
+
+});
