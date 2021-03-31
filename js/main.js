fed0b9157f3587f1e52ddd97e9552c4b2ab5619e
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-03-31 17:23:46 -0400

    added event listener to change classes

--- js/main.js
+++ js/main.js
@@ -21,3 +21,40 @@ formSelector.addEventListener('submit', function (event) {
   formSelector.reset();
 
 });
+
+function addEntry(entry) {
+
+  var divOne = document.createElement('div');
+  divOne.setAttribute('class', 'dummy-row');
+  var divImage = document.createElement('div');
+  divImage.setAttribute('class', 'dummy-image');
+  divOne.appendChild(divImage);
+  var listOne = document.createElement('li');
+  divImage.appendChild(listOne);
+  var photo = document.createElement('img');
+  photo.setAttribute('src', entry.url);
+  photo.setAttribute('class', 'd-photo');
+  photo.setAttribute('alt', 'placeholder');
+  listOne.appendChild(photo);
+  var divText = document.createElement('div');
+  divText.setAttribute('class', 'dummy-text');
+  divOne.appendChild(divText);
+  var listTwo = document.createElement('li');
+  divText.appendChild(listTwo);
+  var heading = document.createElement('h2');
+  heading.textContent = entry.title;
+  listTwo.appendChild(heading);
+  var listThree = document.createElement('li');
+  listThree.textContent = entry.textbox;
+  divText.appendChild(listThree);
+
+  return divOne;
+}
+
+var dummySelector = document.querySelector('.dummy');
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    dummySelector.appendChild(addEntry(data.entries[i]));
+  }
+});
